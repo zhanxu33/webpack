a1e5a7201085c1bcaa22103339538a45326b731a
zhanxu33 <2633745073@qq.com>
2018-08-17 11:34:49 +0800

    去掉无用的js
    去掉无用的js

--- 13.源码分析-源码结构分析/webpack.config.js
+++ 13.源码分析-源码结构分析/webpack.config.js
@@ -1,8 +1,9 @@
-const path = require('path')
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js'
-  }
+const path=require('path');
+module.exports={
+    mode:"development",
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname,'dist'),
+        filename:'bundle.js'
+    }
 }
